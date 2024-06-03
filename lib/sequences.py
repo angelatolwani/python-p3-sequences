85bace336aff609b2e979a721784b5e9e050abd3
Angela Tolwani <angela@tolwani.com>
2024-06-03 16:41:15 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    f0 = 0
+    f1 = 1
+    if length == 0:
+        seq = []
+    elif length == 1:
+        seq = [f0]
+    else:
+        seq = [f0, f1]
+        i = 2
+        while i < length:
+            seq.append(seq[i-1] + seq[i-2])
+            i += 1
+    print(seq)
